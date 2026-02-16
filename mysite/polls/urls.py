668134beefd1e9cc8083baa6289c3c2d39b1424f
Kristian Alonso Diaz Rubio <kristiandiaz2003@gmail.com>
2026-02-16 16:52:04 -0600

    feat: Add dynamic render, Add Views, Add Smart routing

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = "polls"
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<int:question_id>/", views.detail, name="Detail"),
+    path("<int:question_id>/results/", views.results, name="Results"),
+    path("<int:question_id>/vote/", views.vote, name="Vote")
 ]
